f69faee788426fc0d8547ec61cfd16059e0af76e
Juan Vazquez <juanvazquez@gmail.com>
2015-12-19 20:42:39 -0600

    adding comments

--- clojure_advent_of_code/src/clojure_advent_of_code/day4.clj
+++ clojure_advent_of_code/src/clojure_advent_of_code/day4.clj
@@ -2,6 +2,8 @@
 (ns clojure-advent-of-code.day4
   (:require [digest :refer [md5]]))
 
+;; primer-str is the 'abcdef' or 'pqrstuv' being passed in
+;; contraint-str is the '00000' or '000000'
 (defn find-md5-for [primer-str contraint-str]
   (let [candidates (map str (repeat primer-str) (range))]
   (->> candidates
