6392bcafbce327f1fe64b04187bcee2b59ffe461
Juan Vazquez <juanvazquez@gmail.com>
2019-12-10 20:31:06 -0600

    Solved Day 8

--- js_aoc_2019/src/day8.js
+++ js_aoc_2019/src/day8.js
@@ -4,4 +4,34 @@ const breakIntoImageData = function(input, w, h) {
   return input.match(regex);
 };
 
-module.exports = { breakIntoImageData };
+//x and y can be 0,1,2
+const combine = (x, y) => (x === "2" ? y : x);
+
+/**
+ * Combine two arrays by equiv idx's
+ * @param {an array} ary1
+ * @param {an array} ary2
+ */
+const zip = function(ary1 = new Array(), ary2 = new Array()) {
+  let builder = "";
+  for (let idx = 0; idx < ary1.length; idx++) {
+    let combo = combine(ary1[idx], ary2[idx]);
+    builder += combo;
+  }
+  return builder;
+};
+
+// Wish chunking/partitioning was a builtin feature,
+// thanks https://www.w3resource.com/javascript-exercises/fundamental/javascript-fundamental-exercise-265.php
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size)
+  );
+
+//data is the array of array strings
+const createImage = function(layers) {
+  let builder = layers.reduce(zip);
+  return builder;
+};
+
+module.exports = { breakIntoImageData, zip, createImage, chunk };
--- js_aoc_2019/test/day8.test.js
+++ js_aoc_2019/test/day8.test.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const INPUT_FILE = path.join(__dirname, "../resources/input_day8.txt");
-const { breakIntoImageData } = require("../src/day8");
+const { breakIntoImageData, createImage, chunk } = require("../src/day8");
 
 test("Day 1, Check for Corruption", done => {
   fs.readFile(INPUT_FILE, (err, data) => {
@@ -15,6 +15,29 @@ test("Day 1, Check for Corruption", done => {
     });
     const res = answ.match(/1/g).length * answ.match(/2/g).length;
     expect(res).toBe(2904);
+    done();
+  });
+});
+
+test("Day 2 , secret message", done => {
+  fs.readFile(INPUT_FILE, (err, data) => {
+    let layers = chunk(data.toString(), 25 * 6);
+    let img = createImage(layers);
+    const part2 = chunk(img, 25).map(x => {
+      return x
+        .toString()
+        .replace(/0/g, " ")
+        .replace(/1/g, "|");
+    });
+    const ans = [
+      "|  |  ||  |||   ||  |||| ",
+      "|  | |  | |  | |  | |    ",
+      "|||| |    |||  |    |||  ",
+      "|  | | || |  | |    |    ",
+      "|  | |  | |  | |  | |    ",
+      "|  |  ||| |||   ||  |    "
+    ];
+    expect(part2).toStrictEqual(ans);
     done();
   });
 });
