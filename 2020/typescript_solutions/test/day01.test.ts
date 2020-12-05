9b2dc7ee0ae0157c51ce14ba2c3849ff5b5243da
Juan Vazquez <juanvazquez@gmail.com>
2020-12-04 21:01:43 -0600

    add test dir

---
+++ 2020/typescript_solutions/test/day01.test.ts
@@ -0,0 +1,10 @@
+import { solvepart1, solvepart2 } from '../src/day01';
+
+
+test('day1', () => {
+  expect(solvepart1()).toBe(866436)
+});
+test('day2', () => {
+  expect(solvepart2()).toBe(276650720)
+});
+
---
+++ 2020/typescript_solutions/test/day03.test.ts
@@ -0,0 +1,12 @@
+import { solvepart1, solvepart2 } from '../src/day03';
+
+
+test('day3', () => {
+  expect(solvepart1).toBe(171)
+
+})
+
+test('day3-part2', () => {
+  expect(solvepart2).toBe(1206576000)
+
+})
